96c8cf33a81ee73e09e5824f5aae3732a53b3c9f
Harveer-kalkat <harrykalkat@gmail.com>
2020-08-26 04:03:07 +0530

    Updates

---
+++ precache-manifest.9600a2de79c5ecbd6f0e34da9a01c90e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f3f726ee01ab7bb1ba1b45b1f20b39",
+    "url": "/document/index.html"
+  },
+  {
+    "revision": "b612f1b5ee584ab1b7b3",
+    "url": "/document/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "22a8f90ef39dbab874b5",
+    "url": "/document/static/css/main.e03754d4.chunk.css"
+  },
+  {
+    "revision": "b612f1b5ee584ab1b7b3",
+    "url": "/document/static/js/2.3e119bd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/document/static/js/2.3e119bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a8f90ef39dbab874b5",
+    "url": "/document/static/js/main.25ba8e54.chunk.js"
+  },
+  {
+    "revision": "1f706c028aa43c77add5",
+    "url": "/document/static/js/runtime-main.fb316517.js"
+  }
+]);
